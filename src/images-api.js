2ee2162648e972af5ded1a2e0ef904caf8975633
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2024-04-24 08:52:11 +0200

    Update images-api.js

--- src/images-api.js
+++ src/images-api.js
@@ -5,8 +5,15 @@ const KEY = "I8M3Tskhexos2hlxxS2fNs3Zp93TK6TN6fzmKp6vzbE";
 axios.defaults.baseURL = `https://api.unsplash.com/`;
 
 export const fetchImages = async (topic, page) => {
-  const response = await axios.get(
-    `search/photos?query=${topic}&client_id=${KEY}&per_page=20&page=${page}`
-  );
+  const response = await axios.get('', {
+            params: {
+                query: topic, 
+                page: page,
+                per_page: 20, 
+                client_id: KEY, 
+                orientation:'landscape' ,
+    },
+｝）；
+
   return response.data;
 };
