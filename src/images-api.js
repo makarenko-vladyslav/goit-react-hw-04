e554757c98c5ebd04670cc96d8a21cf9c306314d
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2024-04-24 09:13:30 +0200

    Update images-api.js

--- src/images-api.js
+++ src/images-api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const KEY = "I8M3Tskhexos2hlxxS2fNs3Zp93TK6TN6fzmKp6vzbE";
 
-axios.defaults.baseURL = `https://api.unsplash.com/`;
+axios.defaults.baseURL = `https://api.unsplash.com/search/photos`
 
 export const fetchImages = async (topic, page) => {
   const response = await axios.get('', {
